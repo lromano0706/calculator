770de3c8713501b55582319eb59a1235ef8a7b92
lromano0706 <lromano0706@me.com>
2021-12-11 22:56:58 -0800

    calculator upload

---
+++ calculator.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 3000;
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/bmicalculator', (req, res) => {
+  res.sendFile(__dirname + '/bmiCalculator.html');
+});
+
+app.post('/', (req, res) => {
+  var num1 = Number(req.body.num1);
+  var num2 = Number(req.body.num2);
+
+  var result = num1 + num2;
+  res.send(`The result of the calculation is ${result}`);
+});
+
+app.post('/bmiCalculator', (req, res) => {
+  var weight = parseFloat(req.body.weight);
+  var height = parseFloat(req.body.height);
+
+  var result = weight / Math.pow(height, 2);
+  res.send(`Your BMI index is: ${result}`);
+});
+app.listen(port, () => {
+  console.log(`Server started on Port: ${port}`);
+});
